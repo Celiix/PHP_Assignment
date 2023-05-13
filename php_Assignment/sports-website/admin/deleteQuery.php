30cd2e1fb53fbbbb6a62ca09143d964aa382762d
Celiix <92170983+Celiix@users.noreply.github.com>
2023-05-13 12:17:30 +0100

    Delete php_Assignment/sports-website directory

--- php_Assignment/sports-website/admin/deleteQuery.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include '../db.php';
-	if (isset($_POST['Del_id'])) {
-	$query = "DELETE FROM `contact` WHERE `id` = ".$_POST['Del_id']."";
-		if ($db->query($query)) {
-     echo "Successfully Deleted";		}
-	}
-	?>
